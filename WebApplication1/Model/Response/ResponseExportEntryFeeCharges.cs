028d5b15ff615c1d7ec553937972c18cff91a450
interdev <interdev@wizardcomm.net>
2025-07-11 10:18:25 +0530

    changes

---
+++ WebApplication1/Model/Response/ResponseExportEntryFeeCharges.cs
@@ -0,0 +1,17 @@
+﻿namespace SezApi.Model.Response
+{
+	public class ResponseExportEntryFeeCharges
+	{
+
+		public decimal TotalEntryValue { get; set; }
+		public string? EntrySacCode { get; set; }
+		public decimal CGSTper { get; set; }
+		public decimal SGSTper { get; set; }
+		public decimal IGSTper { get; set; }
+		public decimal EntryCGSTAmount { get; set; }
+		public decimal EntrySGSTAmount { get; set; }
+		public decimal EntryIGSTAmount { get; set; }
+		public decimal TotalEntryAmt { get; set; }
+		
+	}
+}
