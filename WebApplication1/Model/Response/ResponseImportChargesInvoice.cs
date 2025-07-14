e326071663de91587bf305c12f19529d1c0df7fd
Kawshikr1999 <kawshikr1999@example.com>
2025-07-15 00:35:01 +0530

    done

--- WebApplication1/Model/Response/ResponseImportChargesInvoice.cs
+++ WebApplication1/Model/Response/ResponseImportChargesInvoice.cs
@@ -6,52 +6,55 @@ namespace SezApi.Model.Response
     {
         // Header Info
         [Key]
+        // Company Info
         public string? CompanyName { get; set; }
         public string? CompanyAddress { get; set; }
         public string? EmailAddress { get; set; }
         public string? CWCGSTNO { get; set; }
 
+        // Invoice Info
         public string? InvNo { get; set; }
-        public DateTime? InvDate { get; set; }
+        public DateTime InvDate { get; set; }
+        public string? PlaceOfSupply { get; set; }
+        public bool IsService { get; set; }
 
+        // Party Info
         public string? PartyName { get; set; }
         public string? PartyAddress { get; set; }
         public string? PartyGST { get; set; }
         public string? StateName { get; set; }
         public string? StateCode { get; set; }
 
-        public string? PlaceOfSupply { get; set; }
-        public bool? IsService { get; set; }
-        public string? PayerName { get; set; }
-        public string? Remarks { get; set; }
-        public string? PrintedBy { get; set; }
-		public DateTime?  ArrivalDate { get; set; }
-
-		// Container Info
-		public string? ICDNo { get; set; }
+        // Container / Entry Info
+        public string? ICDNo { get; set; }
         public string? ContainerCBTNo { get; set; }
         public string? Size { get; set; }
-        public bool Reefer { get; set; }
+        public int? Reefer { get; set; }
         public string? OBLHBLNo { get; set; }
         public string? CargoType { get; set; }
         public int? NoOfPackage { get; set; }
-        public decimal? GrWt { get; set; }
+        public decimal GrWt { get; set; }
         public DateTime? DoValidateDate { get; set; }
+        public DateTime? ArrivalDate { get; set; }
 
-        // Charge Info
+        // Charges
         public string? ChargeCode { get; set; }
         public string? Descripton { get; set; }
         public string? SACCode { get; set; }
+        public decimal Rate { get; set; }
+        public decimal TaxableAmt { get; set; }
+        public decimal CGSTRate { get; set; }
+        public decimal CGSTAmt { get; set; }
+        public decimal SGSTRate { get; set; }
+        public decimal SGSTAmt { get; set; }
+        public decimal IGSTRate { get; set; }
+        public decimal IGSTAmt { get; set; }
+        public decimal Total { get; set; }
 
-        public decimal? Rate { get; set; }
-        public decimal? TaxableAmt { get; set; }
-        public decimal? CGSTRate { get; set; }
-        public decimal? CGSTAmt { get; set; }
-        public decimal? SGSTRate { get; set; }
-        public decimal? SGSTAmt { get; set; }
-        public decimal? IGSTRate { get; set; }
-        public decimal? IGSTAmt { get; set; }
-        public decimal? Total { get; set; }
+        // Misc
+        public string? PayerName { get; set; }
+        public string? Remarks { get; set; }
+        public string? PrintedBy { get; set; }
     }
 
     public class ResponseImportChargesInvoice
@@ -71,7 +74,7 @@ namespace SezApi.Model.Response
         public string? StateCode { get; set; }
 
         public string? PlaceOfSupply { get; set; }
-        public bool? IsService { get; set; }
+        public bool IsService { get; set; }
         public string? PayerName { get; set; }
         public string? Remarks { get; set; }
 		public DateTime? ArrivalDate { get; set; }
@@ -83,12 +86,12 @@ namespace SezApi.Model.Response
     {
         public string? ICDNo { get; set; }
         public string? ContainerCBTNo { get; set; }
+        public int? Reefer { get; set; }
         public string? Size { get; set; }
-        public bool? Reefer { get; set; }
         public string? OBLHBLNo { get; set; }
         public string? CargoType { get; set; }
         public int? NoOfPackage { get; set; }
-        public decimal? GrWt { get; set; }
+        public decimal GrWt { get; set; }
         public DateTime? DoValidateDate { get; set; }
         public List<ChargeDetailDto> Charges { get; set; } = new();
     }
@@ -98,18 +101,18 @@ namespace SezApi.Model.Response
         public string? Descripton { get; set; }
         public string? SACCode { get; set; }
 
-        public decimal? Rate { get; set; }
-        public decimal? TaxableAmt { get; set; }
+        public decimal Rate { get; set; }
+        public decimal TaxableAmt { get; set; }
 
-        public decimal? CGSTRate { get; set; }
-        public decimal? CGSTAmt { get; set; }
+        public decimal CGSTRate { get; set; }
+        public decimal CGSTAmt { get; set; }
 
-        public decimal? SGSTRate { get; set; }
-        public decimal? SGSTAmt { get; set; }
+        public decimal SGSTRate { get; set; }
+        public decimal SGSTAmt { get; set; }
 
-        public decimal? IGSTRate { get; set; }
-        public decimal? IGSTAmt { get; set; }
+        public decimal IGSTRate { get; set; }
+        public decimal IGSTAmt { get; set; }
 
-        public decimal? Total { get; set; }
+        public decimal Total { get; set; }
     }
 }
